db0c8a91982476ddfa904cc507e52f97c4381542
congnguyen-framas <68525123+congnguyen-framas@users.noreply.github.com>
2022-07-09 10:23:36 +0700

    up

---
+++ CreatedMd5Code/CreatedMd5Code/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CreatedMd5Code
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //ma hoa
+        private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                txtDecryptContent.Text = EncodeMD5.EncryptString(txtEncryptContent.Text, txtEncryptPass.Text);
+            }));
+        }
+
+        //giai ma
+        private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                txtEncryptContent.Text = EncodeMD5.DecryptString(txtDecryptContent.Text, txtDecryptPass.Text);
+            }));
+        }
+    }
+}
